4ae4b8f304c73dd8716c4f53593c2146363fff62
Yusuf Oyinlola <yuzstackinnovation@gmail.com>
2025-08-22 12:26:25 +0100

    Refactor the code and fix some bugs

---
+++ script.js
@@ -0,0 +1,90 @@
+'use strict';
+
+let computerScore = 0;
+let humanScore = 0;
+
+function getComputerChoice() {
+  const computerChoice = Math.floor(Math.random() * 3); // Randomly returns a number between 0, 1 or 2
+
+  if (computerChoice === 0) {
+    return 'Rock';
+  } else if (computerChoice === 1) {
+    return 'Paper';
+  } else {
+    return 'Scissors';
+  }
+}
+
+const rockButton = document.querySelector('#rock-button');
+const paperButton = document.querySelector('#paper-button');
+const scissorsButton = document.querySelector('#scissors-button');
+
+rockButton.addEventListener('click', () => {
+  const computerSelection = getComputerChoice();
+  playRound('Rock', computerSelection);
+  /* // Alternatively..
+  playRound("Rock", getComputerChoice()) */
+});
+
+paperButton.addEventListener('click', () => {
+  const computerSelection = getComputerChoice();
+  playRound('Paper', computerSelection);
+});
+
+scissorsButton.addEventListener('click', () => {
+  const computerSelection = getComputerChoice();
+  playRound('Scissors', computerSelection);
+});
+
+const resultsDiv = document.querySelector('#results');
+
+function playRound(humanChoice, computerChoice) {
+  const human = humanChoice.toLowerCase();
+  const computer = computerChoice.toLowerCase();
+
+  if (human === computer) {
+    resultsDiv.textContent = "It's a tie!";
+  } else if (
+    (human === 'rock' && computer === 'scissors') ||
+    (human === 'paper' && computer === 'rock') ||
+    (human === 'scissors' && computer === 'paper')
+  ) {
+    resultsDiv.textContent = `You win! ${human} beats ${computer}`;
+    humanScore++;
+  } else {
+    resultsDiv.textContent = `You lose! ${computer} beats ${human}`;
+    computerScore++;
+  }
+
+  playerScoreSpan.textContent = humanScore;
+  compuerScoreSpan.textContent = computerScore;
+
+  if (humanScore === 5 || computerScore === 5) {
+    const winner =
+      humanScore === 5 ? 'You won the game!' : 'Computer won the game!';
+    resultsDiv.textContent += `, ${winner}`;
+
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorsButton.disabled = true;
+  }
+}
+
+const playerScoreSpan = document.querySelector('#player-score');
+const compuerScoreSpan = document.querySelector('#computer-score');
+
+const resetButton = document.querySelector('#reset-button');
+
+resetButton.addEventListener('click', () => {
+  humanScore = 0;
+  computerScore = 0;
+
+  resultsDiv.textContent = '';
+
+  playerScoreSpan.textContent = humanScore;
+  compuerScoreSpan.textContent = computerScore;
+
+  rockButton.disabled = false;
+  paperButton.disabled = false;
+  scissorsButton.disabled = false;
+});
